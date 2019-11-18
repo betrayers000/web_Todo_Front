4a7bd273b9960f5072de997fcc08145224ae72c3
jw <betrayers000@gmail.com>
2019-11-18 13:54:37 +0900

    login component add

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="login-div col-6 offset-3">
+    <div class="form-group">
+      <label for="id">ID</label>
+      <input id="id" type="text" class="form-control">
+    </div>
+    <div class="form-group">
+      <label for="password">PASSWORD</label>
+      <input id="password" type="password" class="form-control">
+    </div>
+    <button>Login</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+  <h1>Login Page</h1>
+  <login-form/>
+  </div>
+</template>
+
+<script>
+import LoginForm from '../components/LoginForm.vue'
+export default {
+  components:{
+    LoginForm
+  },
+}
+</script>
+
+<style>
+
+</style>
