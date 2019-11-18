27a840629ad89068ff5adf57212606d2f867d7e7
jw <betrayers000@gmail.com>
2019-11-18 14:12:47 +0900

    login form add

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,19 +1,35 @@
 <template>
-  <div class="login-div col-6 offset-3">
-    <div class="form-group">
-      <label for="id">ID</label>
-      <input id="id" type="text" class="form-control">
+  <div>
+    <div v-if="loading" class="spinner-border" role="status">
+      <span class="sr-only">Loading...</span>
     </div>
-    <div class="form-group">
-      <label for="password">PASSWORD</label>
-      <input id="password" type="password" class="form-control">
+    <div v-else class="login-div col-6 offset-3">
+      <div class="form-group">
+        <label for="id">ID</label>
+        <input id="id" type="text" class="form-control" v-model="credentail.username">
+      </div>
+      <div class="form-group">
+        <label for="password">PASSWORD</label>
+        <input id="password" type="password"  class="form-control" v-model="credentail.password">
+      </div>
+      <button class="btn btn-primary" @click="login">Login</button>
     </div>
-    <button>Login</button>
   </div>
 </template>
 
 <script>
 export default {
+  data: function(){
+    return {
+      credentail: {},
+      loading: false,
+    }
+  },
+  methods: {
+    login() {
+      console.log("login")
+    }
+  }
 
 }
 </script>
