51b407c37519a858814e78c754c47d006c038871
jw <betrayers000@gmail.com>
2019-11-19 14:21:41 +0900

    TodoList.vue add

---
+++ src/components/TodoList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Todo</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name : 'TodoList',
+  data : function(){
+    return {
+      todos: [],
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
-
+    <TodoList/>
   </div>
 </template>
 
 <script>
 import router from '../router'
+import TodoList from '../components/TodoList.vue'
 export default {
   name: 'home',
   components : {
-
+    TodoList,
   },
   methods: {
     checkLoggedIn(){
