a40a7faf522aaac692bf085d0502a16309ef95e6
jw <betrayers000@gmail.com>
2019-11-19 15:35:18 +0900

    TodoList & todo delete

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,18 +1,48 @@
 <template>
   <div>
     <h1>Todo</h1>
+    <div class="card my-1" v-for="todo in todos" :key="todo.id">
+      <div class="card-body d-flex justify-content-between">
+        <span>{{todo.title}}</span>
+        <span @click="deleteTodo(todo)">🗑️</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name : 'TodoList',
-  data : function(){
-    return {
-      todos: [],
+  props:{
+    todos: Array,
+  },
+  methods: {
+    deleteTodo(todo){
+      this.$session.start()
+      const token = this.$session.get('jwt')
+      const requestHeader = {
+        headers: {
+          Authorization : "JWT " + token
+        }
+      }
+      console.log(todo.id)
+      axios.delete(`http://localhost:8000/api/v1/todos/${todo.id}/`, requestHeader)
+      .then(res=>{
+        console.log(res)
+        const targetTodo = this.todos.find(function(el){
+          return el === todo
+        })
+        const idx = this.todos.indexOf(targetTodo) // indexOf 값이 없으면 -1이 리턴된다.
+        if (idx > -1){
+          this.todos.splice(idx, 1)
+        }
+      }).catch(err=>{
+        console.log(err)
+      })
     }
   }
-
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,24 @@
 <template>
   <div>
-    <TodoList/>
+    <TodoList :todos="todos"/>
   </div>
 </template>
 
 <script>
 import router from '../router'
 import TodoList from '../components/TodoList.vue'
+import jwtDecode from 'jwt-decode'
+import axios from 'axios'
+
 export default {
   name: 'home',
   components : {
     TodoList,
+  },
+  data: function(){
+    return {
+      todos: [],
+    }
   },
   methods: {
     checkLoggedIn(){
@@ -19,10 +27,30 @@ export default {
         // redirect login page
         router.push('/login')
       }
-    }
+    },
+    getTodos(){
+      this.$session.start()
+      const token = this.$session.get('jwt')
+      const decodedToken = jwtDecode(token)
+      const userId = decodedToken.user_id
+
+      const requestHeader = {
+        headers: {
+          Authorization : "JWT " + token
+        }
+      }
+
+      console.log(`get ${decodedToken.username}'s todo_list`)
+      axios.get(`http://localhost:8000/api/v1/users/${userId}/`, requestHeader).then(res=>{
+        this.todos = res.data.todo_set
+      }).catch(err=>{
+        console.log(err)
+      })
+    },
   },
   mounted: function(){
     this.checkLoggedIn()
+    this.getTodos()
   },
 }
 </script>
